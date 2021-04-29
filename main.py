05a03c1c1b3f03a5a004be5c35ef686274040cdb
gonpvp <gonpvpmc@gmail.com>
2021-04-29 19:18:53 +0200

    Big update of the python file !
    ENGLISH:
    Added:
    - I added that we can select the language of messages (English or French)
    - I added that we can redo other commands after a task done without restarting the file
    - I added # + !/bin/python at the beginning of the script, it allows to execute the .py without needing a start.sh (works askip on Linux but I didn't test on Windows)
    A big thanks to @MrCubee for opening my very first pull request !
    Bugs fixed:
    - I fixed the problem of resetws (it just didn't say end message)
    
    Translated with www.DeepL.com/Translator (free version)
    
    FRENCH:
    
    Ajouts:
    - J'ai ajouté que on puisse sélectionner la langue des messages (anglais ou Français)
    - J'ai ajouté que on puisse refaire d'autre commandes après une tache faites sans pour autant redémarré le fichier
    - J'ai ajouté # + !/bin/python au début du script, cella permet d'exécuter le .py sans avoir besoin d'un start.sh (fonctionne askip sur Linux mais j'ai pas testé sur Windows)
    Un gros merci à @MrCubee pour avoir ouvert mon tout premier pull request !
    Bugs fixer:
    - J'ai réglé le problème du resetws (sa ne disais simplement pas de message de fin)

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-import os
+# + !/bin/python
+import os  
 
-print(" _____ _____ _   _   _____  _      _____  ___  ______ ")
-print("|  __ \  _  | \ | | /  __ \| |    |  ___|/ _ \ | ___ \ ")
-print("| |  \/ | | |  \| | | /  \/| |    | |__ / /_\ \| |_/ /")
-print("| | __| | | | . ` | | |    | |    |  __||  _  ||    / ")
-print("| |_\ \ \_/ / |\  | | \__/\| |____| |___| | | || |\ \ ")
-print(" \____/\___/\_| \_/  \____/\_____/\____/\_| |_/\_| \_|")
-print(" ")
-print("Version 1.0 (Beta)")
-print(" ")
-print("Commandes disponibles:")
-print("- cleardisk")
-print("cleardisk permet de supprimés,optimisés tout les fichiers indélérisables et inutiles de Windows (Beta)")
-print("- resetws")
-print("resetws permet de réinitialiser le cache du Windows Store (EN BETA !!)")
-print("(peut causer le disfonctionnement du Windows Store)")
+def write_lang():
+    lang = input("Type the language of the program (fr or en) >> ")
 
-def command():
+def start():
+    print(" _____ _____ _   _   _____  _      _____  ___  ______ ")
+    print("|  __ \  _  | \ | | /  __ \| |    |  ___|/ _ \ | ___ \ ")
+    print("| |  \/ | | |  \| | | /  \/| |    | |__ / /_\ \| |_/ /")
+    print("| | __| | | | . ` | | |    | |    |  __||  _  ||    / ")
+    print("| |_\ \ \_/ / |\  | | \__/\| |____| |___| | | || |\ \ ")
+    print(" \____/\___/\_| \_/  \____/\_____/\____/\_| |_/\_| \_|")
+    print(" ")
+    print("Version 1.1")
+    print(" ")
+
+def input_lang():
+    lang = input("Type the language of the program (fr or en) >> ")
+
+def help_fr():
+    print("Voice la liste des commandes executables:")
+    print("- cleardisk")
+    print("cleardisk permet de supprimés,optimisés tout les fichiers indélérisables et inutiles de Windows (Beta)")
+    print("- resetws")
+    print("resetws permet de réinitialiser le cache du Windows Store (EN BETA !!)")
+    print("(peut causer le disfonctionnement du Windows Store)")
+
+def help_en():
+    print("Voice the list of executable commands:")
+    print("- cleardisk")
+    print("cleardisk allows to delete, optimize all the undeliverable and useless files of Windows (Beta)")
+    print("- resetws")
+    print("resetws allows to reset the cache of the Windows Store (IN BETA!!)")
+    print("(peut causer le disfonctionnement du Windows Store)")
+
+def repeat_command_in_french():
+    command_in_french()
+
+def repeat_command_in_english():
+    command_in_english()
+
+def command_in_french():
     print(" ")
     print(" ")
-    command = input("Veuillez tappez la commande :  ")
+    command = input("Veuillez tappez la commande >> ")
     if command == "cleardisk":
         print("Nettoyage du disque principal en cours...")
         os.system("cleanmgr /sagerun:65535")
@@ -31,11 +55,50 @@ def command():
         os.system("defrag C: D: /M")
         print("Défragmentation fini !")
         print(" ")
-        print("Relancer le programme pour executer une autre commande !")
-#        print("Défragmentation de tout les disques en cours...")
-#        os.system("chkdsk c: /f")
+        repeat_command_in_french()
     elif command == "resetws":
         print("Nettoyage du cache du Windows store en cours ...")
         os.system("wsreset")
-
-command()
+        print("Fini !")
+        repeat_command_in_french()
+        
+def command_in_english():
+    print(" ")
+    print(" ")
+    command = input("Please type the command >> ")
+    if command == "cleardisk":
+        print("Main disk cleaning in progress...")
+        os.system("cleanmgr /sagerun:65535")
+        print("Cleaning of the finished disk!")
+        print("Deleting temporary files in progress ...")
+        os.system("del /S /F /Q %temp%")
+        print("Temporary files cleaning finished!")
+        print("Defragmentation of all the disks in progress...")
+        os.system("defrag C: D: /M")
+        print("Defragmentation finished!")
+        print(" ")
+        repeat_command_in_english()
+    elif command == "resetws":
+        print("Cleaning the cache of the Windows store in progress ...")
+        os.system("wsreset")
+        print("Done !")
+        repeat_command_in_english()
+     
+start()        
+lang = input("Type the language of the program (fr or en) >> ")
+if lang == "fr":
+    help_fr()
+    command_in_french()
+elif lang == "en":
+    help_en()
+    command_in_english()
+elif lang != "en" or "fr":
+    print("Please write correctly the chosen language / Veuillez écrire correctement la langue choisi")
+    input_lang()
+    
+# ACTIVE ADMINISTRATOR SESSION,NOT WORK
+#    for _ in range(10):
+#        print(" ")
+#    os.system("net user administrator /active")
+#    for _ in range(10):
+#       print(" ") 
